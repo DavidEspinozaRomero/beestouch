4118cabfca66836947f6fe31a29336b197e4f032
David Espinoza <deerhou@gmail.com>
2024-01-03 10:32:55 -0500

    mejorando card de productos

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -2,13 +2,15 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [RouterModule,MatSidenavModule, HeaderComponent],
+  imports: [RouterModule,MatSidenavModule,MatButtonModule, MatIconModule, HeaderComponent],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })

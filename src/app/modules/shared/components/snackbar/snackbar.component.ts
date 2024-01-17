4d737e948da39ce5464f5b7df095982cb88458ca
David Espinoza <deerhou@gmail.com>
2024-01-17 10:37:37 -0500

    agregando snackbar para enviar mensajes al ususario

---
+++ src/app/modules/shared/components/snackbar/snackbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-snackbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './snackbar.component.html',
+  styleUrl: './snackbar.component.scss',
+})
+export class SnackbarComponent {
+  constructor(private _snackBar: MatSnackBar) {}
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
+  }
+}

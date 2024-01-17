115438deea969508bc9193d7546a4952d47bd461
David Espinoza <deerhou@gmail.com>
2024-01-17 10:38:24 -0500

    creando un maquetado inicial de detalle del producto

--- src/app/modules/products/detail-product/detail-product.component.ts
+++ src/app/modules/products/detail-product/detail-product.component.ts
@@ -1,30 +1,39 @@
 import { Component, inject } from '@angular/core';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { CartService } from '../../payments';
+import { CurrencyPipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-detail-product',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule],
+  imports: [
+    CurrencyPipe,
+    TitleCasePipe,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './detail-product.component.html',
   styleUrl: './detail-product.component.scss',
 })
 export class DetailProductComponent {
-  value = '1';
   product = {
     id: 1,
-    title: 'shampoo',
+    title: 'shampoo de miel y gengibre',
     price: 5,
     quantity: 1,
     total: 5,
-    image: '',
-    description: 'shampoo de miel y gengibre',
+    image: '../../../../../assets/imgs/logo.jpg',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veritatis deserunt eius in earum quod non eligendi, culpa dignissimos commodi suscipit tempora accusantium perspiciatis labore distinctio aspernatur fugiat voluptatem vitae? Inventore officiis dolorum hic illum placeat, eaque quo consequatur, vel eum repudiandae voluptate doloribus ut officia pariatur ipsa adipisci deserunt ipsum aliquid! At ab nostrum nihil dicta possimus hic voluptatem.',
   };
   cartService = inject(CartService);
   addProductToCart(product: any) {
